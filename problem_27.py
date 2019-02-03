561888feb8e6f86c2ccb101ae2e889c798f8e692
Nimrod Sadeh <nsadeh@users.noreply.github.com>
2019-02-03 14:30:24 -0500

    Expanded docstring in well_formed

--- problem_27.py
+++ problem_27.py
@@ -1,4 +1,14 @@
-"""daily code problem 27."""
+"""
+daily code problem 27.
+
+Problem: given a string consisting of round,
+curly, or square brackets ("(", "{", "["),
+output whether the string is well-formed (i.e.,
+all openining brackets are closed and a bracket
+open before another is closed after)
+
+This question was asked by Facebook.
+"""
 
 
 import queue
@@ -18,6 +28,28 @@ def well_formed(in_str):
     Input can contain only round, curly, or square brackets.
     Function tests for whether each opening closes and
     whether sentenced close in correct order.
+    
+    This problem is solved by using a LIFO queue. 
+    If the bracket is an opening action (left),
+    then put to queue. If closing, it is only allowed to 
+    close a bracket of its type, or otherwise the string
+    is now well formed. Additionally, if the string is well-formed
+    then the queue must be empty at the end. This solution
+    is O(N).
+    
+    Example 1: input "([])" (well-formed)
+    1. "(" put: queue("(")
+    2. "[" put: queue("(", "[")
+    3. "]" get: queue("(")
+    4. ")" get: queue()
+    Queue is empty: return True
+    
+    Example 2: input "((()" (not well-formed)
+    1. "(" put: queue("(")
+    2. "(" put: queue("(", "(")
+    1. "(" put: queue("(", "(", "(")
+    4. ")" get: queue("(", "(")
+    Queue in not empty: return False
 
     :param in_str: a string consisting of brackets (str)
     :return: True if in_str is well-formed, else false
